4ca8d785c5c0a22bf0be1e4e32ca860c6c709e75
simonholmes001 <simonholmesabc@gmail.com>
2023-06-30 16:03:53 +0200

    minor changes

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,14 @@ if __name__ == "__main__":
     vector_db_constructor = VectorDBConstructor(
         data_source=data_source,
         pdf_path=pdf_path, 
-        embedding="openai", 
-        db="chroma"
+        embedding=embedding, 
+        db=db
         )
     pdf_list = vector_db_constructor.create_pdf_list(pdf_path=pdf_path)
     pdf_read = vector_db_constructor.read_pdf(pdf_list, data_source=data_source)
     pdf_chunk = vector_db_constructor.chunk_pdf(pdf_read)
-    pdf_vector = vector_db_constructor.create_vectorDB(pdf_chunk, data_source=data_source, embedding=embedding, db=db)
+    pdf_vector = vector_db_constructor.create_vectorDB(
+        pdf_chunk, 
+        data_source=data_source, 
+        embedding=embedding, 
+        db=db)
