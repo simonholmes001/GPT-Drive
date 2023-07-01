5a9aa115e28956af3c743c0f2073469e8fb94754
simonholmes001 <simonholmesabc@gmail.com>
2023-07-01 12:16:43 +0200

    functions for chain and prompt creation and minor modifications

--- backend/src/run_chain.py
+++ backend/src/run_chain.py
@@ -1,6 +1,7 @@
-from langchain.chains.question_answering import load_qa_chain
+from langchain.memory import ConversationBufferMemory
 
-from mosaicml_set_up import MosaicmlSetUp
+from chain_setup import Chain
+from mosaicml_setup import MosaicmlSetUp
 
 # Device set-up
 device = f"cuda:{cuda.current_device()}" if cuda.is_available() else "cpu"
@@ -9,6 +10,11 @@ device = f"cuda:{cuda.current_device()}" if cuda.is_available() else "cpu"
 model_type = ["mosaicml/mpt-30b-chat"]
 task_types = ["text_generation"]
 
+# Chain parameters
+chain_types = ["stuff", "refine", "map_reduce", "map_rerank"]
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+# Run parameters
 model = "mosaicml-30B" # Choices are "mosaicml-30B", "openai", "ChatOpenAI" -> TO DO: make into a command line variable
 
 if __name__ == "__main__":
@@ -22,20 +28,20 @@ if __name__ == "__main__":
         model_setup = mosaicml_setp.model_setup(device=device, model_type=model_type)
         stopping_criteria = mosaicml.stopping_criteria()
         llm = mosaicml.generate_text(task_type=task_type, stopping_criteria=stopping_criteria)
-
     if model == "openai":
         pass
-
     if model == "ChatOpenAI":
         pass
+
+    chain = Chain()
+    langchain = chain.never_break_the_langchain(llm=llm, chain_type=chain_type[0], prompt=prompt)
 
     print("\n")
     print("--- START ---")
     print("\n")
     print(f"Model type: {model}")
     print("\n")
-    chain = load_qa_chain(llm=llm, chain_type=parameters[1], prompt=PROMPT)
-    chain.run(input_documents=docs, question=query, products=products, language_one=language_one, language_two=language_two,
+    langchain.run(input_documents=docs, question=query, products=products, language_one=language_one, language_two=language_two,
           language_three=language_three, return_only_outputs=True, memory=memory, verbose=True)
     print("\n")
     print("--- FIN ---")
